dc822068f4d6ece334bdbd1098d74417dda79a39
Sina <sina.it@gmail.com>
2011-05-05 12:16:04 +0430

    some updates

--- src/sns_dhcp/DHCPPacket.java
+++ src/sns_dhcp/DHCPPacket.java
@@ -13,22 +13,22 @@ import java.net.InetAddress;
  * @author sina
  */
 public class DHCPPacket {
-    public byte op ;           // for server = 2 , for client = 1
-    public byte htype;         // hardware kind ,for example 10 Mb Ethernet
-    public byte hlen;          // length of device address which is connected , usually 6 (byte) for MAC
-    public byte hops;          // 0 in end systems , inc for each router
-    public byte [] xid;        // 4 byte message id (repeat)
-    public byte [] secs;       // 2 byte (0)
-    public byte [] flags;      // 2 byte (bit flag = 0)
-    public byte [] ciaddr;     // 4 byte (0)
-    public byte [] yiaddr;     // 4 byte (client ip address)
-    public byte [] siaddr;     // 4 byte (server ip address)
-    public byte [] giaddr;     // 4 byte (0)
-    public byte [] chaddr;     // 16 byte -> first 6 byte = client MAC Address;
-    public byte [] sname;      // 44 byte (0)
-    public byte [] file;       // 128 byte (0)
-    public byte [] magicCookie= new byte[]{99,(byte)130,83,99};
-    public byte [] options;    // 64x byte
+    private byte op ;           // for server = 2 , for client = 1
+    private byte htype;         // hardware kind ,for example 10 Mb Ethernet
+    private byte hlen;          // length of device address which is connected , usually 6 (byte) for MAC
+    private byte hops;          // 0 in end systems , inc for each router
+    private byte [] xid;        // 4 byte message id (repeat)
+    private byte [] secs;       // 2 byte (0)
+    private byte [] flags;      // 2 byte (bit flag = 0)
+    private byte [] ciaddr;     // 4 byte (0)
+    private byte [] yiaddr;     // 4 byte (client ip address)
+    private byte [] siaddr;     // 4 byte (server ip address)
+    private byte [] giaddr;     // 4 byte (0)
+    private byte [] chaddr;     // 16 byte -> first 6 byte = client MAC Address;
+    private byte [] sname;      // 44 byte (0)
+    private byte [] file;       // 128 byte (0)
+    private byte [] magicCookie= new byte[]{99,(byte)130,83,99};
+    private byte [] options;    // 64x byte
     ////////////////////////////////////////////////////////
     //////////////////////// methods ///////////////////////
     ////////////////////////////////////////////////////////
@@ -52,5 +52,220 @@ public class DHCPPacket {
         options = new byte [64];
 
     }
+
+    /**
+     * @return the giaddr
+     */
+    public byte[] getGiaddr() {
+        return giaddr;
+    }
+
+    /**
+     * @param giaddr the giaddr to set
+     */
+    public void setGiaddr(byte[] giaddr) {
+        this.giaddr = giaddr;
+    }
+
+    /**
+     * @return the op
+     */
+    public byte getOp() {
+        return op;
+    }
+
+    /**
+     * @param op the op to set
+     */
+    public void setOp(byte op) {
+        this.op = op;
+    }
+
+    /**
+     * @return the htype
+     */
+    public byte getHtype() {
+        return htype;
+    }
+
+    /**
+     * @param htype the htype to set
+     */
+    public void setHtype(byte htype) {
+        this.htype = htype;
+    }
+
+    /**
+     * @return the hlen
+     */
+    public byte getHlen() {
+        return hlen;
+    }
+
+    /**
+     * @param hlen the hlen to set
+     */
+    public void setHlen(byte hlen) {
+        this.hlen = hlen;
+    }
+
+    /**
+     * @return the hops
+     */
+    public byte getHops() {
+        return hops;
+    }
+
+    /**
+     * @param hops the hops to set
+     */
+    public void setHops(byte hops) {
+        this.hops = hops;
+    }
+
+    /**
+     * @return the xid
+     */
+    public byte[] getXid() {
+        return xid;
+    }
+
+    /**
+     * @param xid the xid to set
+     */
+    public void setXid(byte[] xid) {
+        this.setXid(xid);
+    }
+
+    /**
+     * @return the secs
+     */
+    public byte[] getSecs() {
+        return secs;
+    }
+
+    /**
+     * @param secs the secs to set
+     */
+    public void setSecs(byte[] secs) {
+        this.secs = secs;
+    }
+
+    /**
+     * @return the flags
+     */
+    public byte[] getFlags() {
+        return flags;
+    }
+
+    /**
+     * @param flags the flags to set
+     */
+    public void setFlags(byte[] flags) {
+        this.flags = flags;
+    }
+
+    /**
+     * @return the ciaddr
+     */
+    public byte[] getCiaddr() {
+        return ciaddr;
+    }
+
+    /**
+     * @param ciaddr the ciaddr to set
+     */
+    public void setCiaddr(byte[] ciaddr) {
+        this.ciaddr = ciaddr;
+    }
+
+    /**
+     * @return the yiaddr
+     */
+    public byte[] getYiaddr() {
+        return yiaddr;
+    }
+
+    /**
+     * @param yiaddr the yiaddr to set
+     */
+    public void setYiaddr(byte[] yiaddr) {
+        this.yiaddr = yiaddr;
+    }
+
+    /**
+     * @return the siaddr
+     */
+    public byte[] getSiaddr() {
+        return siaddr;
+    }
+
+    /**
+     * @param siaddr the siaddr to set
+     */
+    public void setSiaddr(byte[] siaddr) {
+        this.siaddr = siaddr;
+    }
+
+    /**
+     * @return the chaddr
+     */
+    public byte[] getChaddr() {
+        return chaddr;
+    }
+
+    /**
+     * @param chaddr the chaddr to set
+     */
+    public void setChaddr(byte[] chaddr) {
+        this.chaddr = chaddr;
+    }
+
+    /**
+     * @return the sname
+     */
+    public byte[] getSname() {
+        return sname;
+    }
+
+    /**
+     * @param sname the sname to set
+     */
+    public void setSname(byte[] sname) {
+        this.sname = sname;
+    }
+
+    /**
+     * @return the file
+     */
+    public byte[] getFile() {
+        return file;
+    }
+
+    /**
+     * @param file the file to set
+     */
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    /**
+     * @return the options
+     */
+    public byte[] getOptions() {
+        return options;
+    }
+
+    /**
+     * @param options the options to set
+     */
+    public void setOptions(byte[] options) {
+        this.options = options;
+    }
+
+    /**
+     * @param xid the xid to set
+     */
+  
    
 }
